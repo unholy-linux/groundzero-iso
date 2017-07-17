06b18f573d8d6a88bda8f87c33a8b01ea3122db7
theringmaster <gzroms@hotmail.com>
2017-07-17 15:14:08 -0400

    Additions and subtractions pt 2

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -9,9 +9,6 @@ ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
 usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
-useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/zsh liveuser
-#chmod 700 /root
-chown -R liveuser:users /home/liveuser
 
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
@@ -21,14 +18,16 @@ sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
-systemctl enable gdm-plymouth.service
+useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/zsh liveuser
+#chmod 700 /root
+chown -R liveuser:users /home/liveuser
+
+systemctl enable lightdm.service
+systemctl set-default graphical.target
 systemctl enable pacman-init.service choose-mirror.service NetworkManager.service bluetooth.service
-systemctl set-default multi-user.target
-pacman -Syy
 systemctl enable org.cups.cupsd.service
 systemctl enable ntpd.service acpid.service
 systemctl enable sshd
-rm /etc/udev/rules.d/81-dhcpcd.rules
 
 export _EDITOR=nano
 echo "EDITOR=${_EDITOR}" >> /etc/environment
--- airootfs/tmp2/repo.sh
+++ airootfs/tmp2/repo.sh
@@ -1,12 +1,72 @@
 #!/bin/bash
 
-mkdir ~/bin
-PATH=~/bin:$PATH
-curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo &&chmod a+x ~/bin/repo
-chmod a+x ~/bin/repo
+ CL_BLU="\033[34m"
+ CL_RST="\033[0m"
 
-ln -s /usr/bin/python2 ~/bin/python
-ln -s /usr/bin/python2-config ~/bin/python-config
+echo -e ${CL_BLU}"    _____     ______    _____       __     __  ${CL_BLU}";
+echo -e ${CL_BLU}"   / ___ \   (____  )  (_   _)     (_ \   / _) ${CL_BLU}";
+echo -e ${CL_BLU}"  / /   \_)      / /     | |         \ \_/ /   ${CL_BLU}";
+echo -e ${CL_BLU}" ( (  ____   ___/ /_     | |          \   /    ${CL_BLU}";
+echo -e ${CL_BLU}" ( ( (__  ) /__  ___)    | |   __     / _ \    ${CL_BLU}";
+echo -e ${CL_BLU}"  \ \__/ /    / /____  __| |___) )  _/ / \ \_  ${CL_BLU}";
+echo -e ${CL_BLU}"   \____/    (_______) \________/  (__/   \__) ${CL_RST}";
+echo "GZLx build environment finalizer. Internet connection required! Make sure all steps are completed.";
 
-yaourt -S android-sdk --noconfirm
-yaourt -S android-sdk-platform-tools --noconfirm
+while true
+do 
+  PS3='Select options in order:'
+  options=("Create Needed Symlinks" "Get Android SDK" "Set Global Name" "Set Global Email" "Quit")
+  select opt in "${options[@]}"
+do
+    case $opt in
+     "Create Needed Symlinks")
+        echo "Installing repo and symlinking python2.";
+        mkdir ~/bin
+        PATH=~/bin:$PATH
+        curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo &&chmod a+x  ~/bin/repo
+        chmod a+x ~/bin/repo
+        ln -s /usr/bin/python2 ~/bin/python
+        ln -s /usr/bin/python2-config ~/bin/python-config
+        clear 
+        break;
+        ;;
+     "Get Android SDK")
+        echo "Downloading and installing Android-SDK.";
+        yaourt -S android-sdk --noconfirm
+        yaourt -S android-sdk-platform-tools --noconfirm
+        clear 
+        break;
+        ;;
+    "Set Global Name")
+        export username="$1"
+        if [ -z $username ];
+        then
+        echo -e "Please enter a username"
+        read username
+        fi
+        git config --global user.name "${username}"
+        echo "Done.";
+        sleep 1
+        clear 
+        break;
+        ;;
+    "Set Global Email")
+        export username="$1"
+        if [ -z $username ];
+        then
+        echo -e "Please enter your git email"
+        read username
+        fi
+        git config --global user.email "${username}"
+        echo "Done.";
+        sleep 1
+        clear 
+        break;
+        ;;
+     "Quit")
+        exit 
+        ;;
+        *) echo invalid option;;
+    esac
+done
+done
